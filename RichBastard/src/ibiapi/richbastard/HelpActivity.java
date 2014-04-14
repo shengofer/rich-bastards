43d0bb054c32d8eae07d52cda9eafd12520cf4c2
antoshka412 <antoshka412@gmail.com>
2014-04-14 10:35:02 +0300

    smth

---
+++ RichBastard/src/ibiapi/richbastard/HelpActivity.java
@@ -0,0 +1,14 @@
+package ibiapi.richbastard;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_help);
+		}
+}
